ca4a804784b2cee5d2651cefbc6d0054d21bb478
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2019-09-05 11:11:00 +0200

    Profile: structure and style

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onClick={handleSubmit}>
+        <Input name="name" placeholder="Full Name" />
+        <Input name="email" type="email" placeholder="Email" />
+
+        <hr />
+
+        <Input type="password" name="oldPassword" placeholder="Old Password" />
+        <Input type="password" name="password" placeholder="New Password" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm Password"
+        />
+
+        <button type="submit">Save</button>
+      </Form>
+
+      <button type="button">Delete Account</button>
+    </Container>
+  );
 }
